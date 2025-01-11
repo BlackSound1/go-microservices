b88b3174e5106abe0b0801205fcf421c1a2791f0
BlackSound1 <matthewr@videotron.ca>
2025-01-10 19:16:03 -0500

    Added stub listener service with RabbitMQ

---
+++ listener-service/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+	// Try to connect to RabbitMQ
+
+	// Start listening for messages
+
+	// Create a consumer to consume messages from queue
+
+	// Watch queue and consume events
+}
